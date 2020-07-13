fd83f1634d1f5c5299ef9ecf0a71522fd2fe58cf
Jeovanni <jeovannildg@gmail.com>
2020-07-12 19:41:50 -0600

    added first click event on the header title

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+//when clicking on the header, the font will turn red.
+const header = document.querySelector(".logo-heading");
+
+header.addEventListener('click', () => {
+    header.style.color = "red";
+});
+
+//when doubleclicking the image, it will display the word "fun"
+
+const busImg = document.querySelector("img")
+console.log(busImg);
